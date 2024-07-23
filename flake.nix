5ede03c697ed850c728581e3a01f6b59ae188bae
m-kuzmin <71077087+m-kuzmin@users.noreply.github.com>
2024-07-23 21:35:10 +0300

    ci: Use flake.nix to manage dependencies

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        hugo dart-sass];
+    };
+  };
+}
